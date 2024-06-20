63fe593bda20ab166a4aa042845f7fd9117e7be5
Your Name <you@example.com>
2024-06-20 23:28:09 +0700

    tung

---
+++ Readme.md
@@ -0,0 +1,52 @@
+# Phần mềm phân tích doanh thu bán hàng 
+## NHÓM 6
+
+Với chủ đề là viết phân tích doanh thu bán hàng, nhóm phát triển với 2 chức năng là lọc doanh thu theo ngày tháng năm bắt đầu và biểu thị tổng doanh thu theo tháng qua các năm bằng các loại biểu đồ phổ biến
+
+## Công nghệ sử dụng
+
+- HTML
+- Python ( Pandas, Flask)
+- Javascript (jquery, chartjs)
+
+## Hướng dẫn cài đặt
+
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+pip install Flask
+pip install pandas
+python main.py
+```
+Sau khi phần mềm đã chạy
+Truy cập vào đường dẫn: http://127.0.0.1:5000/
+
+## License
+
+MIT
+
+**Cảm ơn!**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [dill]: <https://github.com/joemccann/dillinger>
+   [git-repo-url]: <https://github.com/joemccann/dillinger.git>
+   [john gruber]: <http://daringfireball.net>
+   [df1]: <http://daringfireball.net/projects/markdown/>
+   [markdown-it]: <https://github.com/markdown-it/markdown-it>
+   [Ace Editor]: <http://ace.ajax.org>
+   [node.js]: <http://nodejs.org>
+   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+   [jQuery]: <http://jquery.com>
+   [@tjholowaychuk]: <http://twitter.com/tjholowaychuk>
+   [express]: <http://expressjs.com>
+   [AngularJS]: <http://angularjs.org>
+   [Gulp]: <http://gulpjs.com>
+
+   [PlDb]: <https://github.com/joemccann/dillinger/tree/master/plugins/dropbox/README.md>
+   [PlGh]: <https://github.com/joemccann/dillinger/tree/master/plugins/github/README.md>
+   [PlGd]: <https://github.com/joemccann/dillinger/tree/master/plugins/googledrive/README.md>
+   [PlOd]: <https://github.com/joemccann/dillinger/tree/master/plugins/onedrive/README.md>
+   [PlMe]: <https://github.com/joemccann/dillinger/tree/master/plugins/medium/README.md>
+   [PlGa]: <https://github.com/RahulHP/dillinger/blob/master/plugins/googleanalytics/README.md>
---
+++ main.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Đọc file CSV khi server khởi động
+df = pd.read_csv('file.csv')
+
+# Chuyển đổi cột 'Order Date' sang định dạng datetime
+df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%y %H:%M', errors='coerce')
+
+# Chuyển đổi cột 'Quantity Ordered' và 'Price Each' sang dạng số
+df['Quantity Ordered'] = pd.to_numeric(df['Quantity Ordered'], errors='coerce')
+df['Price Each'] = pd.to_numeric(df['Price Each'], errors='coerce')
+
+# Thêm cột 'Total Price' tính tổng tiền cho mỗi đơn hàng
+df['Total Price'] = df['Quantity Ordered'] * df['Price Each']
+
+def calculate_total_revenue(start_date, end_date):
+    # Chuyển đổi start_date và end_date sang định dạng datetime
+    start_date = pd.to_datetime(start_date, format='%d/%m/%Y')
+    end_date = pd.to_datetime(end_date, format='%d/%m/%Y')
+
+    # Lọc dữ liệu trong khoảng thời gian
+    filtered_df = df[(df['Order Date'] >= start_date) & (df['Order Date'] <= end_date)]
+
+    # Tính tổng doanh thu
+    total_revenue = filtered_df['Total Price'].sum()
+    return total_revenue
+
+def calculate_monthly_revenue(year):
+    # Lọc dữ liệu theo năm được chọn
+    filtered_df = df[df['Order Date'].dt.year == year]
+
+    # Tính tổng doanh thu theo từng tháng
+    monthly_revenue = filtered_df.resample('MS', on='Order Date')['Total Price'].sum().reset_index()
+
+    # Chuyển đổi định dạng ngày tháng của cột 'Order Date' thành tháng/năm
+    monthly_revenue['Order Date'] = monthly_revenue['Order Date'].dt.strftime('%m/%Y')
+
+    return monthly_revenue
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    total_revenue = None
+    selected_year = None
+    monthly_revenue = None
+
+    if request.method == 'POST':
+        # Xử lý khi submit form để tính tổng doanh thu theo khoảng thời gian
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        total_revenue = calculate_total_revenue(start_date, end_date)
+        # Trả về dữ liệu dạng JSON để JavaScript xử lý
+        return jsonify({'total': total_revenue})
+
+    else:
+        # Mặc định hiển thị doanh thu hàng tháng cho năm hiện tại
+        selected_year = pd.Timestamp.now().year
+        monthly_revenue = calculate_monthly_revenue(selected_year)
+
+    return render_template('index.html', total_revenue=total_revenue, selected_year=selected_year, monthly_revenue=monthly_revenue)
+
+@app.route('/chart-data', methods=['GET'])
+def chart_data():
+    selected_year = int(request.args.get('year'))
+    monthly_revenue = calculate_monthly_revenue(selected_year)
+
+    # Chuẩn bị dữ liệu cho biểu đồ Chart.js
+    months = monthly_revenue['Order Date'].tolist()
+    revenue_data = monthly_revenue['Total Price'].tolist()
+
+    # Tạo một dictionary chứa dữ liệu để trả về dưới dạng JSON
+    chart_data = {
+        'months': months,
+        'revenue_data': revenue_data
+    }
+
+    return jsonify(chart_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
